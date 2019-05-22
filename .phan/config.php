570c36a946ebb14f040657f3a787091de12c6f04
Umherirrender <umherirrender_de.wp@web.de>
2019-05-22 21:40:45 +0200

    Add phan
    Change-Id: I90c6ead8783b60bb92a93650a2c2e6ef2b5d3445

---
+++ .phan/config.php
@@ -0,0 +1,23 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/PageImages',
+		'../../extensions/Wikibase/client',
+		'../../extensions/Wikibase/lib',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/PageImages',
+		'../../extensions/Wikibase/client',
+		'../../extensions/Wikibase/lib',
+	]
+);
+
+return $cfg;
