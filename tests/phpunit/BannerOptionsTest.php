b24690bef11b49fc665313a43fb2be85eac10faf
Max Semenik <maxsem.wiki@gmail.com>
2019-10-21 22:39:05 -0700

    tests: getMock() is deprecated
    Bug: T192167
    Change-Id: I55bf4974025ffc0caa9e5144f66d2b60689ce64d

--- tests/phpunit/BannerOptionsTest.php
+++ tests/phpunit/BannerOptionsTest.php
@@ -168,7 +168,7 @@ class BannerOptionsTest extends MediaWikiTestCase {
 	 * @return Parser Parser object associated with test pages
 	 */
 	protected function createParser( $title, $namespace ) {
-		$parser = $this->getMock( Parser::class );
+		$parser = $this->createMock( Parser::class );
 
 		$parserOutput = new ParserOutput();
 		$parser->expects( $this->any() )->method( 'getOutput' )
